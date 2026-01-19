f3e5f5cb332d14377e4f9531c3cc700aa886041e
Bielzoka7 <vitorgabrielsoaresdasilva3@gmail.com>
2026-01-19 19:53:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const engrenagem = document.querySelector('.engrenagem');
+
+window.addEventListener('scroll', () => {
+  const scroll = window.scrollY;
+  engrenagem.style.transform =
+    `translateY(-50%) rotate(${-scroll * 0.3}deg)`;
+});
 
